2e43917172bdf7d4be6e4b7d88cbe3b46dd587b3
Indrodaecia <71908265+Indrodaecia@users.noreply.github.com>
2024-05-07 06:43:22 -0700

    Finding Disks on Machine

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,37 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use sysinfo::{Disks, System};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-fn main() {
+fn find_drives() {
+    return;
+}
+
+fn build_tauri() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+fn main() {
+    
+
+    let mut sys = System::new_all();
+
+    sys.refresh_all();
+
+    println!("=> disks:");
+    let disks = Disks::new_with_refreshed_list();
+    for disk in &disks {
+        println!("{disk:?}");
+    }
+
+    // build_tauri()
 }
