4450cbe8f84cc7a92c19558e1c1364636dd46c47
Indrodaecia <71908265+Indrodaecia@users.noreply.github.com>
2024-05-11 13:27:26 -0700

    events are broken

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,37 +1,56 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use sysinfo::{Disks, System};
+use sysinfo::Disks;
+use tauri::Manager;
+use std::{fs::read_dir, path::Path};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+
+#[tauri::command]
+async fn get_files(app: tauri::AppHandle, _window: tauri::Window) {
+    let disks = Disks::new_with_refreshed_list();
+
+    for disk in &disks {
+
+        // gets the path to the disk
+        let disk_path: &Path = disk.mount_point();
+
+        // finds children of disk
+        let entries = read_dir(disk_path).unwrap();
+
+        for child in entries {
+
+            let string_path: String = child.unwrap()
+                                           .path()
+                                           .to_str()
+                                           .expect("Error: failed to convert path to str")
+                                           .to_owned();
+
+            println!("Name: {}", string_path);
+
+            let _ = app.emit_all("event1", ());
+
+        }
+    }
+}
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-fn find_drives() {
-    return;
+
+fn main() {
+    build_tauri() 
 }
 
 fn build_tauri() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            get_files
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-}
-
-fn main() {
-    
-
-    let mut sys = System::new_all();
-
-    sys.refresh_all();
-
-    println!("=> disks:");
-    let disks = Disks::new_with_refreshed_list();
-    for disk in &disks {
-        println!("{disk:?}");
-    }
-
-    // build_tauri()
 }
--- src/main.js
+++ src/main.js
@@ -1,18 +1,74 @@
 const { invoke } = window.__TAURI__.tauri;
+//import { emit, listen } from '@tauri-apps/api/event'
+
+//import tauriapi from '@tauri-apps/api'
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 
 let greetInputEl;
 let greetMsgEl;
+let fileListEl;
 
 async function greet() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
   greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+  await invoke("get_files");
+  addChildEntry("orange");
+}
+
+async function addChildEntry(item_name) {
+  const newChild = document.createElement("button");
+
+  newChild.appendChild(document.createTextNode(item_name));
+
+  //document.getElementById("file-list").appendChild(newChild)
+
+  fileListEl.appendChild(newChild);
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
+  greetInputEl = document.querySelector("#greet-text-input");
   greetMsgEl = document.querySelector("#greet-msg");
+  fileListEl = document.querySelector("#file-list");
+
   document.querySelector("#greet-form").addEventListener("submit", (e) => {
     e.preventDefault();
     greet();
+    addChildEntry("blue");
   });
+
+  window.addEventListener("event1", (event) => {
+    event.preventDefault();
+    // addChildEntry(event.payload)
+    addChildEntry("aab");
+  });
+  
+  
+  setupListener();
+  addChildEntry("green");
+
+  emit_event_23();
+  addChildEntry("green2");
 });
+
+
+// import { emit, listen } from '@tauri-apps/api/event'
+
+// listen to the `click` event and get a function to remove the event listener
+// there's also a `once` function that subscribes to an event and automatically unsubscribes the listener on the first event
+async function setupListener() {
+  const unlisten = await listen('click', (event) => {
+    // event.event is the event name (useful if you want to use a single callback fn for multiple event types)
+    // event.payload is the payload object
+    //addChildEntry(event.payload.theMessage);
+    addChildEntry("bbbbbbbbbbb")
+  })
+}
+
+async function emit_event_23() {
+  emit('click', {
+    theMessage: 'Tauri is awesome!',
+  })
+}
+
+
